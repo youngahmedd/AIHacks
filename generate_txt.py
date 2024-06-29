4b084b1ae917fe46a7442cf0275b1c7d1df3bdb6
Molly Xu <mollyxu@Mollys-MacBook-Pro.local>
2024-06-29 14:43:32 -0700

    script to generate txt file via url

--- generate_txt.py
+++ generate_txt.py
@@ -11,38 +11,3 @@ rendered_content = html2text.html2text(html_content)
 file = open('./txt_files/file_text.txt', 'w')
 file.write(rendered_content)
 file.close()
-
-"""
-# Ingest Data
-"""
-
-# %%
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.schema import Document
-
-# %%
-files = os.listdir("../Aihacks/txt_files")
-file_texts = []
-for file in files:
-    with open(f"../Aihacks/txt_files/{file}") as f:
-        file_text = f.read()
-    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=512, chunk_overlap=64, 
-    )
-    texts = text_splitter.split_text(file_text)
-    for i, chunked_text in enumerate(texts):
-        file_texts.append(Document(page_content=chunked_text, 
-                metadata={"doc_title": file.split(".")[0], "chunk_num": i}))
-
-# %%
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-
-# %%
-embeddings = HuggingFaceEmbeddings()
-
-# %%
-vector_store = FAISS.from_documents(
-    file_texts,
-    embedding=embeddings
-)
